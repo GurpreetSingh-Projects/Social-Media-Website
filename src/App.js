40e26fbdb635e4db2bf3cec56c38c14752ce0a41
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-22 23:58:30 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import Profile from "./pages/profile/Profile.jsx";
+import Login from "./pages/login/Login.jsx";
 function App() {
   return (
     <div>
-      <Profile />
+      <Login />
     </div>
   );
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Social Media</h3>
+          <span className="loginDesc">
+            Connect with Friends around the world on Social Media
+          </span>
+        </div>
+
+        <div className="loginRight">
+          <div className="loginBox">
+            <input type="text" placeholder="Email" className="loginInput" />
+            <input type="text" placeholder="Password" className="loginInput" />
+            <button className="loginButton">Login</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              Create a new account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
