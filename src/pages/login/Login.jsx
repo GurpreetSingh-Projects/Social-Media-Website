1d1fcf7d3322b20452bd69eb2ceabfa0e97eb69d
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-25 20:49:56 +0530

    added responsive nature

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,16 +5,20 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Social Media</h3>
-          <span className="loginDesc">
+          <p className="loginLogo">Social Media</p>
+          <p className="loginDesc">
             Connect with Friends around the world on Social Media
-          </span>
+          </p>
         </div>
 
         <div className="loginRight">
           <div className="loginBox">
             <input type="text" placeholder="Email" className="loginInput" />
-            <input type="password" placeholder="Password" className="loginInput" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="loginInput"
+            />
             <button className="loginButton">Login</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
