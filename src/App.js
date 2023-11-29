a4cbe5c49f5f49ec80abbb58ad9fae8db9339c43
GurpreetSingh-Projects <gpsingh02414@gmail.com>
2023-11-29 18:29:39 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import Profile from "./pages/profile/Profile.jsx";
 import Login from "./pages/login/Login.jsx";
 import Register from "./pages/register/register.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
