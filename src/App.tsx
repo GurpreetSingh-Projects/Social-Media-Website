90ed03fbc96fbc74705f718f4116c9de478f18ec
Gurpreet Singh <gpsingh02414@gmail.com>
2025-06-19 23:00:00 +0530

    Refactor code to support Typescript #9, Refactor Codebase #12 Open, Refactor components to support Typescript #10, Remove hash from url #17

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {Home} from "./pages/Home";
+import {Profile} from "./pages/Profile";
+import Login from "./pages/Login";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
+import Register from "./pages/register";
+export const App: React.FC = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
